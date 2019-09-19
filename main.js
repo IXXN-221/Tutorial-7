6f1f69ed93c6702688182f6c003328912764764f
EmilSchroder <eschro66@gmail.com>
2019-09-19 12:03:47 +1200

    solutions branch updated

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 function changeColor(){
-
+    if(document.getElementById('colorChangeBox').style.background != 'red'){
+        document.getElementById('colorChangeBox').style.background = 'red'
+    } else {
+        document.getElementById('colorChangeBox').style.background = 'pink'
+    }
 }
 
 function createAlert(){
-
+    alert('WARNINGWARNINGWARNING')
 }
 
 function crossOff(){
-
+    let checkbox = document.getElementsByTagName('input')[0]
+    if(checkbox.checked){
+        document.getElementById('one').style.textDecoration = 'line-through'
+    } else {
+        document.getElementById('one').style.textDecoration = 'none'
+    }
 }
 
 function printArray(){
     let arrayToPrint = ['Snake', 'Dog' ,'Scorpion', 'Crocodile', 'Hippopotamus']
+    let printOut = arrayToPrint.map(animal =>{
+        return (`<li>${animal}</li>`)
+    })
+
+    document.getElementById('printOutArray').innerHTML = `<ul>${printOut.join('')}</ul>`
 }
 
+let clicks = 0
 function recordClicks(){
-    
+    clicks++
+    console.log(clicks)   
 }
